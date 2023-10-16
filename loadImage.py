7e9bf153357483f4a32abec0dafc254edd9727fa
Krieo <krieolin@gmail.com>
2023-10-16 15:30:14 +0200

    modified class to save images to its own directory

--- loadImage.py
+++ loadImage.py
@@ -11,35 +11,61 @@ def crop_and_preprocess_image(image_path, x1, x2, y1, y2, target_size=(224, 224)
     """
     # Load the image from the file
     image = cv2.imread(image_path)
-
+    print(image.shape)
+    print(" THIS IS THE SHAPE OF THE IMAGE")
     # Check if the loaded image is not empty
     if image is not None and image.size != 0:
         # Attempt to crop the image using the coordinates (x1, x2, y1, y2)
         cropped_image = image[y1:y2, x1:x2]
-
+        print(cropped_image.shape)
+        print(" THIS IS THE SHAPE OF THE IMAGE cropped")
         # Check if the cropped image is not empty
-        if cropped_image is not None and cropped_image.size != 0:
-            # Resize the cropped image to the desired dimensions
-            cropped_image = cv2.resize(cropped_image, target_size)
+        return cropped_image
 
-            # Convert the cropped image to grayscale
-            # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-
-            # Convert the image to float32
-            cropped_image = cropped_image.astype(np.float32)
-
-            # Normalize the pixel values to [0, 1]
-            cropped_image /= 255.0
-
-            # Optionally, you can expand the dimensions to match the input shape expected by the model
-            # For example, if your model expects an input shape of (batch_size, height, width, channels)
-            cropped_image = np.expand_dims(cropped_image, axis=0)  # Adds a batch dimension
-
-            # Now, 'cropped_image' is ready to be used as input for a CNN model.
-            return cropped_image
-
-    # If there's an issue with the image, return None or handle the error as needed
-    return None
+# # This is the original method
+# def crop_and_preprocess_image(image_path, x1, x2, y1, y2, target_size=(224, 224)):
+#     """
+#     This method is used to crop the image so only the mosquito is returned using the bounding box
+#     from the CSV file as reference, it also resizes the image, grayscales, and normalizes it before
+#     returning it.
+#     """
+#     # Load the image from the file
+#     image = cv2.imread(image_path)
+#     print(image.shape)
+#     print(" THIS IS THE SHAPE OF THE IMAGE")
+#     # Check if the loaded image is not empty
+#     if image is not None and image.size != 0:
+#         # Attempt to crop the image using the coordinates (x1, x2, y1, y2)
+#         cropped_image = image[y1:y2, x1:x2]
+#         print(cropped_image.shape)
+#         print(" THIS IS THE SHAPE OF THE IMAGE cropped")
+#         # Check if the cropped image is not empty
+#         if cropped_image is not None and cropped_image.size != 0:
+#             # Resize the cropped image to the desired dimensions
+#             cropped_image = cv2.resize(cropped_image, target_size)
+#             print(cropped_image.shape)
+#             print(" THIS IS THE SHAPE OF THE cropped IMAGE resized")
+#             # Convert the cropped image to grayscale
+#             # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+#
+#             # Convert the image to float32
+#             cropped_image = cropped_image.astype(np.float32)
+#             print(cropped_image.shape)
+#             print(" THIS IS THE SHAPE OF THE IMAGE as float 32")
+#             # Normalize the pixel values to [0, 1]
+#             cropped_image /= 255.0
+#             print(cropped_image.shape)
+#             print(" THIS IS THE SHAPE OF THE IMAGE normalized")
+#             # Optionally, you can expand the dimensions to match the input shape expected by the model
+#             # For example, if your model expects an input shape of (batch_size, height, width, channels)
+#             cropped_image = np.expand_dims(cropped_image, axis=0)  # Adds a batch dimension
+#             print(cropped_image.shape)
+#             print(" THIS IS THE SHAPE OF THE IMAGE with batch dimension")
+#             # Now, 'cropped_image' is ready to be used as input for a CNN model.
+#             return cropped_image
+#
+#     # If there's an issue with the image, return None or handle the error as needed
+#     return None
 
 
 def display_image(cropped_image, class_label="nothing", image_name="nothing"):
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import os
 import fileHandler
 from loadImage import *
-from sklearn.preprocessing import LabelEncoder
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-import cv2
-import random
+
 # Constants for batch processing
 batch_size = 100  # Change this as needed
 
@@ -35,10 +31,7 @@ if __name__ == '__main__':
     image_data_list = fileHandler.read_csv_file(file_path)
     total_images_processed = 0
 
-    # Create a new list to store the items you want to keep
-    new_image_data_list = []
-
-    for i, image_data in enumerate(image_data_list[:100]):
+    for i, image_data in enumerate(image_data_list):
         if image_data.class_label in class_counts:
             processed_image = crop_and_preprocess_image(image_filename + image_data.img_fName,
                                                         image_data.bbx_xtl, image_data.bbx_xbr, image_data.bbx_ytl,
@@ -50,76 +43,23 @@ if __name__ == '__main__':
                 class_counts[image_data.class_label] += 1
                 class_lists[image_data.class_label].append(image_data)
                 total_images_processed += 1
-                new_image_data_list.append(image_data)  # Add to the new list
+
+                directory = f"data/{image_data.class_label}"
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+
+                # Save the image to the directory
+                cv2.imwrite(f"{directory}/{image_data.img_fName}", processed_image)
 
         if (i + 1) % batch_size == 0:
             # Processed one batch, you can perform batch-specific operations here if needed
             print(f"Processed batch {i // batch_size + 1} - Total images processed: {total_images_processed}")
-
-    # Replace image_data_list with the new list
-    image_data_list = new_image_data_list
 
     # Print class counts
     for class_label, count in class_counts.items():
         print(f'Count of {class_label}: {count}')
 
     print(f'Total images processed: {total_images_processed}')
-label_encoder = LabelEncoder()
-class_labels = [data.class_label for data in image_data_list]
-encoded_labels = label_encoder.fit_transform(class_labels)
-one_hot_labels = to_categorical(encoded_labels)
 
-
-X = [data.image for data in image_data_list]
-y = one_hot_labels
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-X_train = np.array(X_train)
-X_test = np.array(X_test)
-
-# Print original class labels
-print("Original Class Labels:")
-print(class_labels)
-
-# Print encoded labels
-print("Encoded Labels:")
-print(encoded_labels)
-
-# Print one-hot encoded labels
-print("One-Hot Encoded Labels:")
-print(one_hot_labels)
-
-# Combine images and labels
-image_label_pairs = list(zip(X_train, y_train))
-
-# Now, image_label_pairs is a list of tuples, where each tuple contains an image (X_train) and its label (y_train).
-
-# Shuffle the data to ensure randomization during training (optional)
-
-random.shuffle(image_label_pairs)
-
-# Unzip the shuffled data back into separate lists for training
-X_train_shuffled, y_train_shuffled = zip(*image_label_pairs)
-
-# Convert to NumPy arrays
-X_train_shuffled = np.array(X_train_shuffled)
-y_train_shuffled = np.array(y_train_shuffled)
-
-# Display the first 5 images with their labels
-for i in range(3):
-    image = (X_train_shuffled[i][0] * 255).astype(np.uint8)  # Convert values back to 0-255 range
-    label = y_train_shuffled[i]
-
-    # Decode the one-hot label back to its original class label
-    original_label = label_encoder.inverse_transform([label.argmax()])[0]
-
-    # Debugging information to check image dimensions and data type
-    print(f"Image {i} - Shape: {image.shape}, Data Type: {image.dtype}")
-
-    # Display the image with its original label
-    cv2.imshow(f'Label: {original_label}', image)
-
-# Wait until a key is pressed and then close all windows
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+print(class_lists)
 
