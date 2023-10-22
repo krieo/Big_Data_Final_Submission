14902966887b9968d7b1c37a8a1950b32c74dbb2
Krieo <krieolin@gmail.com>
2023-10-22 14:31:37 +0200

    formatted file

--- fileHandler.py
+++ fileHandler.py
@@ -14,7 +14,7 @@ def read_csv_file(file_path):
     with open(file_path, mode="r") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            # Create an instance of ImageData for each row in the CSV
+            # This creates an instance of ImageData for each row in the CSV
             image_data = ImageData(row['img_fName'], row['img_w'], row['img_h'],
                                    row['bbx_xtl'], row['bbx_ytl'], row['bbx_xbr'],
                                    row['bbx_ybr'], row['class_label'])
