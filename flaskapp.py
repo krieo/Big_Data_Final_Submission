d3de6f9fc0ef0cd1380ee22e03493b00be18d679
Krieo <krieolin@gmail.com>
2023-10-22 14:43:04 +0200

    got it to print out the probability

--- flaskapp.py
+++ flaskapp.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-app = Flask("This is a sample")
-# Define the folder where you want to save your models
+app = Flask("Mosquito detection")
+# This gets the model that was created in the model folder
 model_folder = "models"
 model_filename = "my_model.h5"
 model_path = os.path.join(model_folder, model_filename)
@@ -29,13 +29,18 @@ def predict():
         normalized_image = resized_image / 255.0
         expanded_image = np.expand_dims(normalized_image, axis=0)
 
-        yhat = model.predict(expanded_image)
-        predicted_class = np.argmax(yhat)
+        predictions = model.predict(expanded_image)
+        predicted_class = np.argmax(predictions)
 
-        class_names = ["Anna", "Bana", "Cana", "Dana", "Eana", "Fana"]
+        class_names = ["Aegypti", "Albopictus", "Anopheles", "Culex", "Culiseta", "Japonicus_Korecius"]
         predicted_label = class_names[predicted_class]
+        probability = 0
+        for i, prediction in enumerate(predictions):
+            predicted_class = class_names[np.argmax(prediction)]
+            probability = max(prediction)
+            print(f"Predicted Class - {predicted_class}, Probability - {probability:.2f}")
 
-        return f"Predicted Class: {predicted_label}"
+        return f"Predicted Class - {predicted_class}, Probability - {probability:.2f}"#return f"Predicted Class: {predicted_label}"
     else:
         return "No file uploaded."
 
